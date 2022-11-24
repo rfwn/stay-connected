b1c890a91fa93fddc06c611f7a26c96fcdecd4db
rfwn࿐ <m.erfan.majidi@Gmail.com>
2022-11-24 17:05:46 +0330

    tes

---
+++ src/parsers/trojan.js
@@ -0,0 +1,1 @@
+const server = 'trojan://NrBbTr9I06@app1.aslibekhar.com:80?security=tls&headerType=none&type=tcp&sni=app1.aslibekhar.com#%40hashmakvpn+%D8%A7%DA%AF%D9%87+%D9%82%D8%B7%D8%B9+%D8%B4%D8%AF%DB%8C+%D8%A8%DB%8C%D8%A7+%D8%A7%DB%8C%D9%86%D8%AC%D8%A7'
